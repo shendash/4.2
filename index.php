74a3e20029ff91e8e8faa36b75bb6750803db418
Дарья <137810221+shendash@users.noreply.github.com>
2023-09-24 18:22:27 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$postData = file_get_contents('php://input'); 
+$data = json_decode($postData, true);   
+echo json_encode($data);
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER["CONTENT_TYPE"] == 'application/json') {
+ header('Content-Type: text/html; charset=utf-8');   
+}
+?>
